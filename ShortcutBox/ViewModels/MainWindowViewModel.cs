f211d55a66cd3491acd20d7fd9cc2e0f721e02a5
Akn <sibata486@gmail.com>
2021-12-31 22:23:26 +0900

    init

---
+++ ShortcutBox/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace ShortcutBox.ViewModels
+{
+    using Prism.Mvvm;
+
+    public class MainWindowViewModel : BindableBase
+    {
+        private string title = "Prism Application";
+
+        public MainWindowViewModel()
+        {
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set { SetProperty(ref title, value); }
+        }
+    }
+}
