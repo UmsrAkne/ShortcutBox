710551d3f68871f0f4c45fd9c5dae34478ad5d74
Akn <sibata486@gmail.com>
2021-12-31 22:40:15 +0900

    add / DragAndDrop 処理用のビヘイビアクラスを追加

---
+++ ShortcutBox/Models/DragAndDropBehavior.cs
@@ -0,0 +1,41 @@
+﻿namespace ShortcutBox.Models
+{
+    using System.IO;
+    using System.Windows;
+    using ViewModels;
+    using Microsoft.Xaml.Behaviors;
+
+    public class DragAndDropuBehavior : Behavior<Window>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            // ファイルをドラッグしてきて、コントロール上に乗せた際の処理
+            AssociatedObject.PreviewDragOver += AssociatedObject_PreviewDragOver;
+
+            // ファイルをドロップした際の処理
+            AssociatedObject.Drop += AssociatedObject_Drop;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.PreviewDragOver -= AssociatedObject_PreviewDragOver;
+            AssociatedObject.Drop -= AssociatedObject_Drop;
+        }
+
+        private void AssociatedObject_Drop(object sender, DragEventArgs e)
+        {
+            // ファイルパスの一覧の配列
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            var vm = ((Window)sender).DataContext as MainWindowViewModel;
+        }
+
+        private void AssociatedObject_PreviewDragOver(object sender, DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.Copy;
+            e.Handled = e.Data.GetDataPresent(DataFormats.FileDrop);
+        }
+    }
+}
