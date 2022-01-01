ee6416f701790f82698416f7567d14a4339aa48f
Akn <sibata486@gmail.com>
2022-01-01 23:56:15 +0900

    add / FileSystemInfo をラップしたクラスを追加

---
+++ ShortcutBox/Models/ExFileInfo.cs
@@ -0,0 +1,17 @@
+﻿namespace ShortcutBox.Models
+{
+    using System.IO;
+
+    public class ExFileInfo
+    {
+        public ExFileInfo(FileSystemInfo fileInfo)
+        {
+            FileSystemInfo = fileInfo;
+            IsDirectory = fileInfo is DirectoryInfo;
+        }
+
+        public FileSystemInfo FileSystemInfo { get; private set; }
+
+        public bool IsDirectory { get; private set; }
+    }
+}
