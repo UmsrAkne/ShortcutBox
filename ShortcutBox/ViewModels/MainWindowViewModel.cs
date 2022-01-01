c004aadf3d1e79bf96936f33a73384abeb98cff7
Akn <sibata486@gmail.com>
2022-01-01 15:47:22 +0900

    add / コンテキストメニューと内部の機能を実装
    選択したファイルのパスをコピーする機能を実装

--- ShortcutBox/ViewModels/MainWindowViewModel.cs
+++ ShortcutBox/ViewModels/MainWindowViewModel.cs
@@ -3,12 +3,17 @@
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.IO;
+    using System.Windows;
+    using Prism.Commands;
     using Prism.Mvvm;
 
     public class MainWindowViewModel : BindableBase
     {
         private string title = "Prism Application";
         private FileSystemInfo selectedFileInfo;
+
+        private DelegateCommand copyFullPathCommand;
+        private DelegateCommand copyParentDirectoryPathCommand;
 
         public MainWindowViewModel()
         {
@@ -23,5 +28,27 @@
         public ObservableCollection<FileSystemInfo> Files { get; private set; } = new ObservableCollection<FileSystemInfo>();
 
         public FileSystemInfo SelectedFileInfo { get => selectedFileInfo; set => SetProperty(ref selectedFileInfo, value); }
+
+        public DelegateCommand CopyFullPathCommand
+        {
+            get => copyFullPathCommand ?? (copyFullPathCommand = new DelegateCommand(() =>
+            {
+                if (SelectedFileInfo != null)
+                {
+                    Clipboard.SetText(SelectedFileInfo.FullName);
+                }
+            }));
+        }
+
+        public DelegateCommand CopyParentDirectoryPathCommand
+        {
+            get => copyParentDirectoryPathCommand ?? (copyParentDirectoryPathCommand = new DelegateCommand(() =>
+            {
+                if (SelectedFileInfo != null)
+                {
+                    Clipboard.SetText(Path.GetDirectoryName(SelectedFileInfo.FullName));
+                }
+            }));
+        }
     }
 }
