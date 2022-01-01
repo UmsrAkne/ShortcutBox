d38d7c1698c52af4762a4c36d468c29ab951d8e4
Akn <sibata486@gmail.com>
2022-01-02 00:02:00 +0900

    fix / FileSystemInfo を使用していた箇所を ExFileInfo に差し替え

--- ShortcutBox/Models/ExFileInfo.cs
+++ ShortcutBox/Models/ExFileInfo.cs
@@ -13,5 +13,11 @@
         public FileSystemInfo FileSystemInfo { get; private set; }
 
         public bool IsDirectory { get; private set; }
+
+        public string Name => FileSystemInfo.Name;
+
+        public string FullName => FileSystemInfo.FullName;
+
+        public bool Exists => FileSystemInfo.Exists;
     }
 }
