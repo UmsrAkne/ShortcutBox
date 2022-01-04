902ef3adbfa3c4720bd9ddd9df49446b7708d275
Akn <sibata486@gmail.com>
2022-01-04 22:57:04 +0900

    add / DBを取り扱うためのクラスを追加

---
+++ ShortcutBox/Models/DBs/FileHistoryDbContext.cs
@@ -0,0 +1,33 @@
+﻿namespace ShortcutBox.Models.DBs
+{
+    using System.Data.SQLite;
+    using System.IO;
+    using Microsoft.Data.Sqlite;
+    using Microsoft.EntityFrameworkCore;
+
+    public class FileHistoryDbContext : DbContext
+    {
+        public DbSet<FileHistory> FIleHistories { get; set; }
+
+        public string DBFileName => "file_history.sqlite";
+
+        public void CreateDatabase()
+        {
+            if (!File.Exists(DBFileName))
+            {
+                Database.EnsureCreated();
+            }
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!File.Exists(DBFileName))
+            {
+                SQLiteConnection.CreateFile(DBFileName);
+            }
+
+            var connectionString = new SqliteConnectionStringBuilder { DataSource = DBFileName }.ToString();
+            optionsBuilder.UseSqlite(new SQLiteConnection(connectionString));
+        }
+    }
+}
